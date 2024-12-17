f5ff6ad8af7a4572237cf6de46ca6bf307f5edff
Dimitris <dimitris.giannoulis@co-opts.com>
2024-12-17 22:17:12 +0000

    Tries out changing next.config.mjs to see if it solves issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'standalone', // Required for Cloud Run compatibility
+    reactStrictMode: true,
+  };
+  
+  export default nextConfig;
+  
