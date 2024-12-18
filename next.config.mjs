4874ef8f1afa0394fda6330e4a72b85648ddc86a
Dimitris <dimitris.giannoulis@co-opts.com>
2024-12-18 00:27:47 +0000

    Tries out changing next.config.mjs to see if it solves issue 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,21 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone', // Required for Cloud Run compatibility
     reactStrictMode: true,
+    async headers() {
+      return [
+        {
+          // Applies the cache control to all pages
+          source: '/(.*)', // All routes
+          headers: [
+            {
+              key: 'Cache-Control',
+              value: 'max-age=60, stale-while-revalidate=300', // Set the Cache-Control header
+            },
+          ],
+        },
+      ]
+    },
   };
   
   export default nextConfig;
